0c173e4ecd40629d7a4fc33003abed1cf63fa943
claudplusplus <claud214villar@gmail.com>
2024-12-09 18:45:21 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,24 @@ export default {
     {
         outDir: '../dist',
         emptyOutDir: true,
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                index: './src/index.html',
+                page1: './src/act11.html',
+                page2: './src/act10.html',
+                page3: './src/act9.html',
+                page4: './src/act8.html',
+                page5: './src/act7.html',
+                page6: './src/act6.html',
+                page7: './src/act4to5.html',
+                page8: './src/act1to3.html',
+                page9: './src/act12.html',
+                page10: './src/act2-1.html',
+                page11: './src/act3.html',
+                page12: './src/act4.html'
+            }
+        }
     },
     plugins:
     [
